47f1a7c6a9e5cff2c37596fca1ad542839571cb7
Jônatas Henrique <jonatshenrique@gmail.com>
2023-07-16 17:18:05 -0300

    Adicionando view home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,7 +3,27 @@ import './index.scss';
 const Home = () => {
   return (
     <section className='Home'>
+      <div className='Home__Container'>
+        <div className='Home__Area-Left'>
+          <div className='Home__My-Name'>
+            <h3>my name is Jônatas</h3>
+          </div>
+          <div className='Home__Title'>
+            <h1>Develope<span>r</span> front end</h1>
+          </div>
+          <div className='Home__Barras'>
+            <div className='Home__Barra-1'></div>
+            <div className='Home__Barra-2'></div>
+            <div className='Home__Barra-3'></div>
+            <div className='Home__Barra-4'></div>
+          </div>
+        </div>
+        <div className='Home__Area-Rigth'>
+          <div className='Home__Area-Perfil'>
 
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
